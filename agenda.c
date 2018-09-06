1420192fc96ba12458db9ce6df5fa12e6927d033
IDSRosler <igorsouzarosler@gmail.com>
2018-09-06 18:00:19 -0300

    Modificação na agenda

--- agenda.c
+++ agenda.c
@@ -4,34 +4,39 @@
 #include<stdlib.h>
 #include<string.h>
 
-/*typedef struct nom{
-    char pessoa=[20];
-}nome;*/
+void *pBuffer;
 
-void incluir();
+typedef struct var{
+	int op,i,cont;
+}variavel;
+
+typedef struct nom{
+    char pessoa[20];
+}nome;
+
+void incluir(variavel *inicio);
 void apagar();
 void buscar();
-void listar();
+void listar(variavel *inicio);
 
 int main(void)
 {
-    void *pBuffer;
-    int *op;
-   // nome *pessoa;
+    variavel *inicio; 
 
-    if((pBuffer = (int *)malloc(1 * sizeof(int)))== NULL){
+    if((pBuffer = (variavel *)malloc(sizeof(variavel)))== NULL){
         printf("Memória inssufuciente!");
         exit(1);
     }
-    op = pBuffer;
+    inicio = pBuffer;
+    inicio->cont = 0;
 
     do{
-        printf("[ 1 ] INCLUIR\n [ 2 ] APAGAR\n [ 3 ] BUSCAR\n [ 4 ] LISTAR");
-        scanf("%d",op);
+        printf("[ 1 ] INCLUIR\n[ 2 ] APAGAR\n[ 3 ] BUSCAR\n[ 4 ] LISTAR\n");
+        scanf("%d",&inicio->op);
 
-        switch (*op){
+        switch (inicio->op){
             case 1:
-                incluir();
+				incluir(inicio);
             break;
             case 2:
                 apagar();
@@ -40,43 +45,47 @@ int main(void)
                 buscar();
             break;
             case 4:
-                listar();
+                listar(inicio);
             break;
             default:
+				free(pBuffer);
                 exit(1);
             }
         }while(1);
 
     return 0;
 }
-void incluir(void** pBuffer,int** op){
-    char* nome;
-    int *tam;
-
-    if((nome = (char *)malloc(20 * sizeof(char)))== NULL){
-        printf("Memória insuficiente!");
+void incluir(variavel *inicio){
+	nome *pessoa;
+	
+	if((pBuffer = (nome *)realloc(pBuffer,sizeof(variavel) + sizeof(nome) + (inicio->cont * sizeof(nome))))== NULL){
+		printf("Memória inssufuciente!");
         exit(1);
-    }
-    if((tam = (int *)malloc(sizeof(int)))== NULL){
-        printf("Memória insuficiente!");
-        exit(1);
-    }
-    printf("Digite o nome: ");
-    scanf("%s",nome);
-
-    *tam = strlen(nome);
-
-     if((nome = (char *)realloc(nome,(*tam) * sizeof(char)))== NULL){
-        printf("Memória insuficiente!");
-        exit(1);
-    }
-    free(tam);
+	}
+	printf("%d\n",inicio->cont);
+	pessoa = pBuffer + sizeof(variavel) + (inicio->cont * sizeof(nome)) ;
+	
+	printf("Digite o nome: ");
+	scanf("%s",pessoa->pessoa);
+	
+	inicio->cont++;
 
 }
 
 void apagar(){
+	printf("teste\n");
 }
+
 void buscar(){
+	printf("teste\n");
 }
-void listar(){
+
+void listar(variavel *inicio){
+	nome *pessoa;
+	pessoa = pBuffer + sizeof(variavel);
+	for(inicio->i = 0; inicio->i < inicio->cont; inicio->i++){
+		printf("%s ",pessoa->pessoa);
+		pessoa++;
+		}
+	printf("\n");
 }
